8d8bfc250f341674964e58bbbed4aaa19c9a0d76
TechnologicNick <technologicnick@gmail.com>
2021-04-26 03:53:54 +0200

    Remove debugging code of scrap-mechanic-common
    Accidentally left that in after testing

--- src/modpackManager.ts
+++ src/modpackManager.ts
@@ -10,18 +10,6 @@ export default class ModpackManager {
     } = {};
 
     static async createModpack(name: string, description: string) {
-        let base = path.join(process.env.APPDATA ?? "AppdataNotFound", "Axolot Games", "Scrap Mechanic", "User");
-
-        let USER_DIR = fs.readdirSync(base) // Get all files and directories
-            .filter(dir => dir.match(/^User_\d+$/g)) // Check if it matches a Steam id
-            .map(dir => path.join(base, dir, "ugccache.json")) // Combine the base, directory name and filename to be checked
-            .filter(file => fs.existsSync(file))
-            .sort((a, b) => fs.statSync(b).mtime.getTime() - fs.statSync(a).mtime.getTime()) // Sort by modified date (high to low)
-            .map(file => path.dirname(file))
-            [0]; // Return the first
-
-        console.log(USER_DIR);
-
         const modpack = new Modpack({ name: name, description: description });
 
         const dir = path.join(Core.getModpacksDirectory(), sanitize(name));
